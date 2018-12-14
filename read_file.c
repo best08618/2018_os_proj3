f9df06c2e2c4ea90f841940e248dc70d184571be
best08618 <best08618@naver.com>
2018-12-14 10:42:28 +0900

    read_file.c indent

--- read_file.c
+++ read_file.c
@@ -67,50 +67,50 @@ int main(void)
 
 
 		for(int l = 0; l< block_num; l ++){
-            struct blocks* bp = &tf.data_blocks[block_store[l]];
-            struct dentry* den;
-            char* sp = bp->d;
-            int read_size = 0 ;
-            while(*sp && read_size < 1024)
+			struct blocks* bp = &tf.data_blocks[block_store[l]];
+			struct dentry* den;
+			char* sp = bp->d;
+			int read_size = 0 ;
+			while(*sp && read_size < 1024)
 			{ //only read 1 data block
-                den = (struct dentry *) sp;
-                sp += den->dir_length;
-                read_size += den->dir_length;
-                printf("file name : %s\n",*den->n_pad);
+				den = (struct dentry *) sp;
+				sp += den->dir_length;
+				read_size += den->dir_length;
+				printf("file name : %s\n",*den->n_pad);
 
-                if(strcmp(*den->n_pad, "file_1") == 0) //to find if file_1 exist
-                {
-                    printf("found %s, enter inode %d\n", *den->n_pad, den->inode);
-                    user_node = den->inode;
-                    user_ptr = &tf.inode_table[user_node];
-                    printf("datablocks size : %dbytes\n", user_ptr -> size);
+				if(strcmp(*den->n_pad, "file_1") == 0) //to find if file_1 exist
+				{
+					printf("found %s, enter inode %d\n", *den->n_pad, den->inode);
+					user_node = den->inode;
+					user_ptr = &tf.inode_table[user_node];
+					printf("datablocks size : %dbytes\n", user_ptr -> size);
 					break;
-                }
-					
-            }
+				}
+
+			}
 			int user_db_size = user_ptr -> size;
 			int block_num = 0 ;
 			int block_store[0x6];
 			while(user_db_size > 0)
 			{
 				block_store[block_num]=user_ptr->blocks[block_num];
-            	printf("data block : %d\n", block_store[block_num]);
-            	block_num ++;
-            	user_db_size = user_db_size - 1000;
+				printf("data block : %d\n", block_store[block_num]);
+				block_num ++;
+				user_db_size = user_db_size - 1000;
 			}
 
 			for(int l = 0; l< block_num; l++)
-        	{
+			{
 				struct blocks* bp = &tf.data_blocks[block_store[l]];
-            	printf("Read block : %d\n",block_store[l]);
+				printf("Read block : %d\n",block_store[l]);
 				for(int a=0; a< 1024; a++)
 				{
 					buffer[a] = bp->d[a];
 				}
 				printf("data : %s\n", buffer);
-        	}		
+			}		
 			break;
-		
+
 		}
 	}
 
